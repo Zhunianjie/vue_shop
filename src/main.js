f6f67a6bcd4b05499c3da356b570f8700b5a26dd
zhuyanjie <751098051@qq.com>
2020-06-22 19:48:03 +0800

    login

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './plugins/element.js'
+import './assets/css/global.css' // 导入全局样式表
 
 Vue.config.productionTip = false
 
