f6609a3a1ff9485bab6f55865f750aa24223c47d
zhuyanjie <19971745123ZhuYj>
2020-06-23 15:12:37 +0800

    login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,21 +6,21 @@
           <img src="../assets/logo.png">
         </div>
         <!--登录表单区域-->
-        <el-form :model="loginForm" class="login_form">
+        <el-form ref="loginFormRef" :model="loginForm" class="login_form" :rules="loginFormRules">
           <!--用户名-->
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input v-model="loginForm.username" prefix-icon="el-icon-user-solid">
             </el-input>
           </el-form-item>
           <!--密码-->
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input  v-model="loginForm.password" prefix-icon="el-icon-lock" type="password">
             </el-input>
           </el-form-item>
           <!--按钮区域-->
           <el-form-item class="btns">
-            <el-button type="primary">登录</el-button>
-            <el-button type="info">重置</el-button>
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info" @click="resetLoginForm">重置</el-button>
           </el-form-item>
         </el-form>
       </div>
@@ -33,9 +33,34 @@ export default {
     return {
       // 这是登录表单的数据绑定对象
       loginForm: {
-        username: 'admin',
+        username: 'zs',
         password: '123456'
+      },
+      // 这是表单的验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    // 点击重置登录表单
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    login () {
+      this.$refs.loginFormRef.validate(valid => {
+        if (!valid) return
+        this.$http.post()
+      })
     }
   }
 }
